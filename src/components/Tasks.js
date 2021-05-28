02d4f1cb79090f96460b5c7b4189631d9b27a0d4
priyankamande <priyankamande24@gmail.com>
2021-05-28 11:07:29 +0530

    code changes

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,9 +1,13 @@
-const Tasks = () => {
-    return (
-        <div>
-            
-        </div>
-    )
+import Task from './Task'
+
+const Tasks = ({ tasks, onDelete, onToggle }) => {
+  return (
+    <>
+      {tasks.map((task, index) => (
+        <Task key={index} task={task} onDelete={onDelete} onToggle={onToggle} />
+      ))}
+    </>
+  )
 }
 
 export default Tasks
