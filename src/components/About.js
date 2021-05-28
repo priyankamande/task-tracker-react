f5efd1d6ee8282f5610bf560c7dba13d14bfcb12
priyankamande <priyankamande24@gmail.com>
2021-05-28 11:12:49 +0530

    code clean up

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 
 const About = () => {
   return (
-    <div>
+    <div style={{textAlign: 'center'}}>
       <h4>Version 1.0.0</h4>
       <Link to='/'>Go Back</Link>
     </div>
